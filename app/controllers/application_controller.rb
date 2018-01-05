83fd7040a83de2a6ad54630f74923062aa3bcaea
Mark Chavez <markchav3z@gmail.com>
2018-01-05 21:10:00 +0800

    add user helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,16 @@ class ApplicationController < ActionController::Base
 
   before_action :set_locale
 
+  def current_user
+    User.find_by(id: session['user_id'])
+  end
+  helper_method :current_user
+
+  def signed_in?
+    !!current_user
+  end
+  helper_method :signed_in?
+
   def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
   end
--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,6 +1,8 @@
 class DashboardsController < ApplicationController
 
   def show
+    dashboard = Dashboard.new(current_user)
+    render "show", locals: { dashboard: dashboard }
   end
 
 end
